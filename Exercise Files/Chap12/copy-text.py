bb7c050531b9ca2a3563b60bdb424213d41e132b
CoryF <coryf65@gmail.com>
2020-07-09 22:32:41 -0500

    more notes !

--- Exercise Files/Chap12/copy-text.py
+++ Exercise Files/Chap12/copy-text.py
@@ -6,7 +6,9 @@ def main():
     infile = open('c:/Users/Cory/Documents/_Code/Python_EssentialTraining/Exercise Files/Chap12/lines.txt', 'rt')
     outfile = open('c:/Users/Cory/Documents/_Code/Python_EssentialTraining/Exercise Files/Chap12/lines-copy.txt', 'wt')
     for line in infile:
-        print(line.rstrip(), file=outfile)
+        print(line.rstrip(), file=outfile) # using print I can change the line endings on the fly
+        #could also use this
+        # outfile.writelines(line)
         print('.', end='', flush=True) # end='' Prevents a new line, flush=True cleaning the output buffer for some OS's
     outfile.close() # closing the output file to prevent data loss
     print('\ndone.')
