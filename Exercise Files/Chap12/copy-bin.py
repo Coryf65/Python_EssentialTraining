ab49013e36939c46ef1c85880b340aeb099205d3
CoryF <coryf65@gmail.com>
2020-07-09 22:36:43 -0500

    notes on copying a binary file

--- Exercise Files/Chap12/copy-bin.py
+++ Exercise Files/Chap12/copy-bin.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+# copies a Binary File
+
 def main():
-    infile = open('berlin.jpg', 'rb')
-    outfile = open('berlin-copy.jpg', 'wb')
+    infile = open('c:/Users/Cory/Documents/_Code/Python_EssentialTraining/Exercise Files/Chap12/berlin.jpg', 'rb')
+    outfile = open('c:/Users/Cory/Documents/_Code/Python_EssentialTraining/Exercise Files/Chap12/berlin-copy.jpg', 'wb')
     while True:
-        buf = infile.read(10240)
+        buf = infile.read(10240) # Don't want to read the entire file 10,000Bytes / 10kb is pretty small, need to know how much system ram you have
         if buf:
             outfile.write(buf)
             print('.', end='', flush=True)
