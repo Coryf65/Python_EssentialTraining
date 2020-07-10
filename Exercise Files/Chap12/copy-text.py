571752f26627d8c8d73463928114e38bd826ca83
CoryF <coryf65@gmail.com>
2020-07-09 22:31:33 -0500

    added some notes

--- Exercise Files/Chap12/copy-text.py
+++ Exercise Files/Chap12/copy-text.py
@@ -7,8 +7,8 @@ def main():
     outfile = open('c:/Users/Cory/Documents/_Code/Python_EssentialTraining/Exercise Files/Chap12/lines-copy.txt', 'wt')
     for line in infile:
         print(line.rstrip(), file=outfile)
-        print('.', end='', flush=True)
-    outfile.close()
+        print('.', end='', flush=True) # end='' Prevents a new line, flush=True cleaning the output buffer for some OS's
+    outfile.close() # closing the output file to prevent data loss
     print('\ndone.')
 
 if __name__ == '__main__': main()
