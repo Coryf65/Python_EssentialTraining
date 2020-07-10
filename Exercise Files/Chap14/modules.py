c18be0a02924299208b3b4e06cab8913d718b3f3
CoryF <coryf65@gmail.com>
2020-07-09 22:47:48 -0500

    using the System module and os

--- Exercise Files/Chap14/modules.py
+++ Exercise Files/Chap14/modules.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-import sys
+import sys, os
 
 def main():
     v = sys.version_info
+    platform = sys.platform
+    system = os.name
+    current_directory = os.getcwd()
+
+
     print('Python version {}.{}.{}'.format(*v))
+    print(f'Your PC is a: {platform} {system}')
+    print(f'Current Working Directory: {current_directory}')
 
 if __name__ == '__main__': main()
